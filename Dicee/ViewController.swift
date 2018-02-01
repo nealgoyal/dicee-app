63638c619e4ba62689015cee114f6e434427f69f
Neal Goyal <ngoya001@ucr.edu>
2018-02-01 04:26:11 -0800

    Added shake motion API

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -41,5 +41,10 @@ class ViewController: UIViewController {
         diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
     }
     
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        updateDiceImages()
+    }
+
+    
 }
 
